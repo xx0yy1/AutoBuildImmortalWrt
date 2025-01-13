13cf8fa0328cba0b5b7750a5485c901f0dd972e7
XiaoYYH <151626433+xx0yy1@users.noreply.github.com>
2025-01-13 15:35:45 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -33,6 +33,7 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
+PACKAGES="$PACKAGES kmod-mt7921e"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
