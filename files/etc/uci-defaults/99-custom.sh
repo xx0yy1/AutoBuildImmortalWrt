addef21833d78e1f6c059f105d5fc264ce83d1cf
XiaoYYH <151626433+xx0yy1@users.noreply.github.com>
2025-01-31 18:50:20 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -6,12 +6,6 @@ echo "Starting 99-custom.sh at $(date)" >> $LOGFILE
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 uci set firewall.@zone[1].input='ACCEPT'
 
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
-
-
 # 计算网卡数量
 count=0
 for iface in /sys/class/net/*; do
